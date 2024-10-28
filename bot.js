3e74edd1d0eeff6d4eb0433b5fda68a05977a3cf
umarrg <umarabubakarfta@gmail.com>
2024-10-28 08:25:36 +0100

    added some fixes

--- bot.js
+++ bot.js
@@ -1,62 +1,84 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
-const cron = require('cron');
+const xml2js = require('xml2js');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text({ type: 'application/atom+xml' }));
+
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
-const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 const YOUTUBE_CHANNEL_ID = process.env.YOUTUBE_CHANNEL_ID;
+const CALLBACK_URL = 'https://995f-197-211-58-96.ngrok-free.app/notifications';
 
 let userChatIds = [];
-let lastVideoId = null;
 
-async function getLatestYouTubeVideo() {
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${YOUTUBE_CHANNEL_ID}&order=date&maxResults=1&type=video&key=${YOUTUBE_API_KEY}`;
+function subscribe() {
+    const hubUrl = 'https://pubsubhubbub.appspot.com/subscribe';
+    const topicUrl = `https://www.youtube.com/xml/feeds/videos.xml?channel_id=${YOUTUBE_CHANNEL_ID}`;
 
-    try {
-        const response = await axios.get(url);
-        const video = response.data.items[0];
-        const videoId = video.id.videoId;
-        const videoTitle = video.snippet.title;
-        const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    const callbackUrl = CALLBACK_URL;
 
-        return {
-            id: videoId,
-            title: videoTitle,
-            url: videoUrl,
-        };
-    } catch (error) {
-        console.error('Error fetching YouTube video:', error.message);
-        return null;
-    }
-}
+    const params = new URLSearchParams();
+    params.append('hub.mode', 'subscribe');
+    params.append('hub.topic', topicUrl);
+    params.append('hub.callback', callbackUrl);
+    params.append('hub.verify', 'async');
 
-async function forwardLatestVideo() {
-    const video = await getLatestYouTubeVideo();
-
-    if (video && video.id !== lastVideoId) {
-        const message = `📹 New Video Posted: *${video.title}*\nWatch here: ${video.url}`;
-
-        userChatIds.forEach(chatId => {
-            bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
+    axios.post(hubUrl, params)
+        .then(response => {
+            console.log('Subscription request sent.');
+        })
+        .catch(error => {
+            console.error('Error subscribing to YouTube notifications:', error.response.data);
         });
-
-        lastVideoId = video.id;
-        console.log(`Forwarded video: ${video.title}`);
-    } else {
-        console.log('No new video to forward.');
-    }
 }
 
-const videoCheckJob = new cron.CronJob('*/5 * * * *', () => {
-    console.log('Checking for new YouTube video...');
-    forwardLatestVideo();
+app.get('/notifications', (req, res) => {
+    const hubMode = req.query['hub.mode'];
+    const hubChallenge = req.query['hub.challenge'];
+
+    if (hubMode === 'subscribe') {
+        console.log('Subscription verified.');
+        res.status(200).send(hubChallenge);
+    } else {
+        res.status(400).send('Bad Request');
+    }
 });
 
-videoCheckJob.start();
+app.post('/notifications', (req, res) => {
+    const xml = req.body;
+
+    xml2js.parseString(xml, (err, result) => {
+        if (err) {
+            console.error('Error parsing XML:', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        try {
+            const entry = result.feed.entry[0];
+            const videoId = entry['yt:videoId'][0];
+            const videoTitle = entry.title[0];
+            const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+
+            const message = `📹 New Video Posted: *${videoTitle}*\nWatch here: ${videoUrl}`;
+
+            userChatIds.forEach(chatId => {
+                bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
+            });
+
+            console.log(`Forwarded video: ${videoTitle}`);
+
+            res.status(200).send('OK');
+        } catch (e) {
+            console.error('Error processing notification:', e);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
@@ -77,9 +99,7 @@ bot.on('message', (msg) => {
     console.log(msg);
 });
 
-app.get('/', (req, res) => {
-    res.send('YouTube Forwarder Bot is running');
-});
+subscribe();
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
